3255e8f3c8fef03b176f07ef4982de7757754d30
Manikandan8056 <manikandan8056@gmail.com>
2019-10-29 16:58:51 +0530

    completed faculty feature

---
+++ src/main/java/com/revature/grademanagementsystemfacultyms/GrademanagementsystemFacultyMsApplication.java
@@ -0,0 +1,16 @@
+package com.revature.grademanagementsystemfacultyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class GrademanagementsystemFacultyMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrademanagementsystemFacultyMsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/revature/grademanagementsystemfacultyms/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package com.revature.grademanagementsystemfacultyms.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	
+	
+}
